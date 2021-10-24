1e5076612148eac2225d7b10c996d80c10bd48f5
rlxone <reflexxblr@gmail.com>
2021-10-25 01:01:52 +0300

    Update

--- script.js
+++ script.js
@@ -28,18 +28,21 @@ replayDiv.onclick = function () {
 };
 
 function checkScroll() {
-  var fraction = 0.8;
-  var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
-  b = y + h, //bottom
-  visibleX, visibleY, visible;
+  if (video.getAttribute("data-start") === "true") {
+    var fraction = 0.8;
+    var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+    b = y + h, //bottom
+    visibleX, visibleY, visible;
 
-  visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-  visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+    visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+    visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
 
-  visible = visibleX * visibleY / (w * h);
+    visible = visibleX * visibleY / (w * h);
 
-  if (visible > fraction) {
-      video.play();
+    if (visible > fraction) {
+        video.setAttribute("data-start", "false");
+        video.play();
+    }
   }
 }
 
